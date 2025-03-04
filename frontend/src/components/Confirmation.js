8ffc7668e533acd7942943ccc541daeb6cdd2fca
afiyaustad <ustadafiya@gmail.com>
2025-03-04 12:01:43 +0530

    Initial commit

---
+++ frontend/src/components/Confirmation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Confirmation = () => (
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+    <div className="text-center p-5 shadow-lg rounded-4 bg-light">
+      <h1 className="text-success fw-bold">✅ Appointment Confirmed!</h1>
+      <p className="text-muted fs-5">We have received your appointment details.</p>
+    </div>
+  </div>
+  
+);
+
+export default Confirmation;
